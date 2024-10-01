233307798530fd9b7171497d26e8229caf80763f
MorenoCreativo <adrielmoreno@hotmail.com>
2024-10-01 08:57:33 +0200

    Completed UI Messages

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  final bool online;
+  final String email;
+  final String name;
+  final String uid;
+
+  User({
+    required this.online,
+    required this.email,
+    required this.name,
+    required this.uid,
+  });
+}
